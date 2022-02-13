9e85f13322e9e3d8fe775ceb3d04ac5c66bde3cc
Chang Yong Lik <theeahlag@gmail.com>
2022-02-13 20:08:02 +0900

    Added circular array

--- cracking-the-code-interview/circular_array/python/circular_array.py
+++ cracking-the-code-interview/circular_array/python/circular_array.py
@@ -1,5 +1,31 @@
 import unittest
 
+class CircularArray(object):
+    
+    def __init__(self, array) -> None:
+        self.array = array
+        self.start = 0
+
+    def rotate(self, i):
+        self.start = (self.start + i) % len(self.array)
+        
+    def __iter__(self):
+        for i in range(self.start, len(self.array)):
+            yield self.array[i]
+        for i in range(0, self.start):
+            yield self.array[i]
+        
+    def __getitem__(self, i):
+        return self.array[(self.start + i) % len(self.array)]
+    
+    def __setitem__(self, i, item):
+        self.array[(self.start + i) % len(self.array)] = item
+
+    def __delitem__(self, i):
+        index = (self.start + i) % len(self.array)
+        del self.array[index]
+        if index < self.start:
+            self.start -= 1
 class Test(unittest.TestCase):
     def test_circular_array(self):
         ca = CircularArray([11, 22, 33, 44, 55, 66, 77])
@@ -33,7 +59,7 @@ class Test(unittest.TestCase):
         array = []
         for item in ca:
             array.append(item)
-        self.asserEqual(array, [11, 999, 44, 55, 66, 77])
+        self.assertEqual(array, [11, 999, 44, 55, 66, 77])
 
 if __name__ == "__main__":
     unittest.main()
