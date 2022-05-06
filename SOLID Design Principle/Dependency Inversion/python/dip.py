a4f5d4b8cb9709ad90431b874f3f6e627ee7aa0c
Chang Yong Lik <theeahlag@gmail.com>
2022-05-06 11:01:34 +0900

    Added DI

---
+++ SOLID Design Principle/Dependency Inversion/python/dip.py
@@ -0,0 +1,52 @@
+from abc import abstractmethod
+from enum import Enum
+
+class Relationship(Enum):
+    PARENT = 0
+    CHILD = 1
+    SIBLING = 2
+
+class Person:
+    
+    def __init__(self, name):
+        self.name = name
+
+class RelationshipBrowser:
+    @abstractmethod
+    def find_all_children_of(self, name):
+        pass
+
+# Low level module
+class Relationships(RelationshipBrowser):
+    
+    def __init__(self):
+        self.relations = []
+    
+    def add_parent_and_child(self, parent, child):
+        self.relations.append(
+            (parent, Relationship.PARENT, child)
+        )
+        self.relations.append(
+            (child, Relationship.CHILD, parent)
+        )
+        
+    def find_all_children_of(self, name):
+        for r in self.relations:
+            if r[0].name == name and r[1] == Relationship.PARENT:
+                yield r[2].name
+
+# High level module
+class Research:
+    def __init__(self, browser):
+        for p in browser.find_all_children_of('John'):
+            print(f'John has a child called {p}')
+
+parent = Person('John')
+child1 = Person('Chris')
+child2 = Person('Matt')
+
+relationships = Relationships()
+relationships.add_parent_and_child(parent, child1)
+relationships.add_parent_and_child(parent, child2)
+
+Research(relationships)
